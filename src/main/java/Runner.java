22de7974bc752d0ac2c865b55cfc36091f95ba02
adeagbot <terry.adeagbo@gmail.com>
2018-10-26 20:34:43 +0100

    Included initial file commits

---
+++ src/main/java/Runner.java
@@ -0,0 +1,17 @@
+package main.java;
+public class Runner {
+	  public static void main(String args[]){
+		 if(args.length!=5){
+			    System.err.println("Incorrect number of arguments passed: "+args.length);
+			    System.out.println("Arguments expected: java -jar gmgmtmapper.jar"
+			    		+ " [input file] "
+			    		+ " [output directory] "
+			    		+ " [scope message scope file] "
+			    		+ " [log directory] "
+			    		+ " [year] ");
+			    System.exit(1);     
+		 }			  
+		 System.setProperty("log.dir",args[3]);
+		 main.scala.MTMapperApp.main(args);
+	  }
+}
