2a98770c261146cc03b2cb69e18f0339cec3604d
adeagbot <terry.adeagbo@gmail.com>
2018-10-26 22:23:06 +0100

    Included sample output

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,7 +3,7 @@ public class Runner {
 	  public static void main(String args[]){
 		 if(args.length!=5){
 			    System.err.println("Incorrect number of arguments passed: "+args.length);
-			    System.out.println("Arguments expected: java -jar gmgmtmapper.jar"
+			    System.out.println("Arguments expected: java -jar mtmapper.jar"
 			    		+ " [input file] "
 			    		+ " [output directory] "
 			    		+ " [scope message scope file] "
